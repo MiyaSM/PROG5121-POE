6b1aec402f9c979f6eb6faadc53fe8e1bcf0c379
MiyaSM <ST10476380@rcconnect.edu.za>
2026-09-17 00:03:21 +0200

    Finalized main method login flow and phone validation

--- login/src/main/java/com/mycompany/login/Register.java
+++ login/src/main/java/com/mycompany/login/Register.java
@@ -19,34 +19,63 @@ public class Register {
     String UserName;
     String Password;
     String cellnumber;
+    String FirstName;
+    String LastName;
     
     
     //ask and collect user inputs
+    System.out.print("Enter FirstName");
+    FirstName = myscanner.nextLine();
+            
+    System.out.print("Enter LastName");
+    LastName= myscanner.nextLine();
     
     System.out.print("Enter username");
     System.out.println("It must contain an under score and is no more than 5 character long ");
     UserName = myscanner.nextLine();
     
     System.out.print("Enter last Password");
-    System.out.println("Password must be lat least 8 charactes long ");
-    System.out.println("Password must contain a number ");
-    System.out.println("Password must Hve a capital letter and special characters ");
+    System.out.print("Password must be lat least 8 charactes long ");
+    System.out.print("Password must contain a number ");
+    System.out.print("Password must Hve a capital letter and special characters ");
     Password = myscanner.nextLine();
     
     System.out.print("Enter South African phone number e.g(+27123456789");
     cellnumber = myscanner.nextLine();
-    
-    
-   
-    
-    
-    
-    
-    
-    
+
+    //validate cellphone number
+       while (!myLogin.checkCellPhoneNumber(cellnumber)) {  
+           System.out.println("Cell phone number incorrectly formatted or does not contain international code.");
+           cellnumber = myscanner.nextLine(); 
+       }
+       System.out.println("Cell phone number successfully added.");
+       
+        // Register user and output result
+        String registered = myLogin.registerUser(UserName, Password, cellnumber, FirstName, LastName);
+        System.out.println("\n" + registered);
+        
+        // Terminate if registration failed
+        if (!registered.contains("You have been successfully registered")){
+        System.out.println("Registration failed. Please restart and try again.");
+        return;
+        }
+        //user loging in
+        System.out.println("Enter username");
+        String loginName = myscanner.nextLine();
+        
+        System.out.println("Enter Password");
+        String myPass = myscanner.nextLine();
+             
+      
+       // Process authentication
+       boolean isloggedIn = myLogin.loginUser(loginName, myPass);
+       String Status = myLogin.returnLoginStatus(isloggedIn);
+       
+       System.out.println("\n" + Status);
+
   
-    
-    
-    
-   }   
+
+   }    
+       
 }
+   
