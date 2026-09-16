da272fcfe1bcda39c1001189797bb6e43ba28931
MiyaSM <ST10476380@rcconnect.edu.za>
2026-09-16 21:00:49 +0200

    Updated login.java Added two new methods
    Added the register userUser methods which will register the user lastly CheckPhoneNumber using regex

--- login/src/main/java/com/mycompany/login/Login.java
+++ login/src/main/java/com/mycompany/login/Login.java
@@ -55,6 +55,49 @@ public class Login {
         }
         //This method ensures that the cell phone is the correct length and contains the international country code.
         
+        public boolean checkCellPhoneNumber(String cellPhoneNumber){
+            if (cellPhoneNumber == null){
+            return false;
+        }
+            /*** Validates South African cell phone numbers with international prefix (+27).
+            * @see <a href="https://stackoverflow.com/questions/33477950/java-regex-phone-number">StackOverflow Regex Reference</a>
+           */
+            String regex = "^\\+27\\d{9}$";
+        return cellPhoneNumber.matches(regex);
+        }
+        
+        public String registerUser(String username, String password, String cellNumber, String firstName, String lastName){
+        //validate userName
+        
+        if (!checkUserName(username)){
+            
+            return "User name is not correctly formitted please ensure that your username contains"
+                    + "an underscore and is no more than five characters in leghth";
+        }
+        
+        if (!checkPasswordComplexity(password)){
+           return "Password is not correctly formated please ensure that the password"
+                   + "contains at least eight characters, a capital letter, a number"
+                   + "and a special character";
+                   
+        }
+        
+        /***  using the 'this' keyword learn't that it  prevent variable shadowing and .
+    * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/thiskey.html">Oracle Java Docs - Using the this Keyword</a>
+    * 
+    */
+        this.registeredUsername = username;
+        this.registeredPassword = password;
+        this.registeredCellNumber = cellNumber;
+        
+         return "You have been successfully registered";
+        }
+        
+        
+        
+        
+        
+        
          
         
         
