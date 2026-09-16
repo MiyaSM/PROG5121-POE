b1d7aa049fec26691debcbbcbf493716fc26369f
MiyaSM <ST10476380@rcconnect.edu.za>
2026-09-16 16:54:03 +0200

    added checkpasswordComplexity and checkusername
    Username Validation: Verifies that the username meets length requirements and contains an underscore.
    
    Password Validation: Ensures passwords are at least 8 characters long and include an uppercase letter, a number, and a special character.

--- login/src/main/java/com/mycompany/login/Login.java
+++ login/src/main/java/com/mycompany/login/Login.java
@@ -17,8 +17,48 @@ public class Login {
         private String registeredUsername;
         private String registeredPassword;
         private String registeredCellNumber;
-        private String firstName;
-        private String lastName;
+        
+        //check if the user name iS NOT an "" ,that it contains Letters a number and special characters
+        public boolean checkUserName(String userName){
+            return !userName.equals("") && userName.contains("_") && userName.length() <= 5;
+        }
+        
+        //Check if passord has 8 characters a capital letter ,special characters and a number
+        public boolean checkPasswordComplexity(String password){
+            if (password.equals("") || password.length() < 8){
+               return false;  
+            }
+           
+            boolean capitalChar = false;
+            boolean Number = false;
+            boolean specialChar = false;
+            
+            //used a for loop to check each character in the password
+            for (int i =0; i < password.length(); i++){
+                char pass = password.charAt(i);
+            
+            //check if it has an uppercase letter
+            if (Character.isUpperCase(pass)){
+                capitalChar = true;
+            }
+            // chck if it contain special character
+           if (!Character.isLetterOrDigit(pass)) {
+                specialChar = true;
+            }
+           //check if it has a number
+            if(Character.isDigit(pass)){
+                Number = true;
+            }
+           }
+            return capitalChar && Number && specialChar;
+              
+        }
+        //This method ensures that the cell phone is the correct length and contains the international country code.
+        
+         
+        
+        
+        
         
         
         
